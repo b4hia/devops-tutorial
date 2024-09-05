13d8b7201e469a48a841c1c20f6ebcc43e27904b
Gabriel Reis <gabrieldreisilva4@gmail.com>
2024-09-05 00:11:16 -0300

    func(huskycicommit): testando padrao de commit2

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,8 @@
-export default { extends: ['@commitlint/config-conventional'],
-		 rules: {
-		 'type-enum':[2, 'always', ['func', 'corr', 'docs', 'refa', 'test', 'perf']],
-		 'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['func', 'corr', 'docs', 'refa', 'test', 'perf']],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
   },
 };
+
